762b35d6ef214d4a0a5c05d83cba2aff427cffa6
VickkyProgrammer <73972088+vickkykruz@users.noreply.github.com>
2022-12-10 13:18:45 -0800

    Learning Angular

--- Lession 13/app.js
+++ Lession 13/app.js
@@ -2,10 +2,13 @@
     'use strict';
 
     angular.module('ShoppingListApp', [])
-    .controller('ShoppingListController', ShoppingListController)
-    .cutomService('cutomService', cutomService);
+    .controller('ShoppingListAddController', ShoppingListAddController)
+    .controller('ShoppingListShowController', ShoppingListShowController)
+    .service('cutomService', cutomService);
 
-    function ShoppingListController() {
+    // Inject the service
+    ShoppingListAddController.$inject = ['cutomService'];
+    function ShoppingListAddController(cutomService) {
         let itemAdder = this;
         
         itemAdder.itemName = "";
@@ -13,7 +16,45 @@
         itemAdder.itemQuantity = ""
 
         itemAdder.addItem = ()=> {
-            
+            cutomService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
         }
+    }
+
+    ShoppingListShowController.$inject = ['cutomService'];
+    function ShoppingListShowController(cutomService){
+        let showList = this;
+
+        // Renove Item
+        showList.removeItem = (itemIndex)=> {
+            cutomService.removeItem(itemIndex);
+        }
+        showList.item = cutomService.getItems();
+    }
+
+    function cutomService() {
+        let service = this;
+
+        let items = [];
+
+        // Service To Add Item
+        service.addItem = (itemName, itemQuantity)=> {
+            let item = {
+                name: itemName,
+                itemQuantity: itemQuantity
+            }
+            items.push(item);
+        }
+
+        // Service to remove item 
+        service.removeItem = (itemIndex)=> {
+            items.splice(itemIndex, 1);
+        }
+
+        // Fetch Items
+        service.getItems = ()=> {
+            return items;
+        }
+
+
     }
 })();
